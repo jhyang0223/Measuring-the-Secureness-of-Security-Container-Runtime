5df46b479a3c35e448a5e540701e25eaa543cc67
root <flash3470@gmail.com>
2020-02-02 23:06:26 +0900

    complete

--- apps/crawling/Crawl.py
+++ apps/crawling/Crawl.py
@@ -94,36 +94,139 @@ def InitExploitDict():
 #available system call in docker
 def GetLinuxSyscallList():
     linux_syscallList = list()
+    #system call string is usually '#define .* __NR_(system_call) .*'
     lFormat = re.compile('.*NR_([0-9a-zA-Z_]*).*')
     
+    #This command get string in library header file
     cmd = '''cat $(printf "#include <sys/syscall.h>\nSYS_read" | gcc -E - | awk '{print $3}' | grep -v -e '^$' | grep -v -e '<' | sed 's/\"//g') | grep "#define" | grep "NR_" | sort -u'''
     catResult = subprocess.check_output(cmd,shell=True).decode().strip("\n")
     catResultIO = io.StringIO(catResult)
-    print(catResult)
+
+    #examine each line fits to my regular expression
     for line in catResultIO.readlines():
         searchRet  = lFormat.search(line.strip("\n"))
         if searchRet is not None:
             linux_syscallList.append(searchRet.group(1))
-
+    print(len(linux_syscallList))
     return linux_syscallList
 
-#find system calls used in the exploit and return used system call list
-def FindSyscallStrList(code):
+#figure system call in function list
+def FigureSyscall(funcStrList, linux_syscallList):
     syscallStrList = list()
-    linux_syscallList = GetLinuxSyscallList()
-    
-    
-    
-    
+
+    for funcStr in funcStrList:
+        for syscall in linux_syscallList:
+            if funcStr.strip(" ") == syscall:
+                syscallStrList.append(syscall)
+
     return syscallStrList
 
-def AddDBExploit2Dict(initDict):
+#find system calls used in the exploit and return used system call list
+def FindCodeSyscallStrList(code,linux_syscallList):
+    funcStrList = list()
+    fFormat=re.compile('''[A-Za-z_][0-9A-Za-z_]+\(.*\)''')
+    for funcStr in fFormat.findall(code):
+        funcStrList.append(funcStr.split("(")[0])
+    
+    fFormat_sys = re.compile('''SYS_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_sys.findall(code):
+        funcStrList.append(funcStr.split("_")[1])
+
+    fFormat_NR = re.compile('''NR_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_NR.findall(code):
+        funcStrList.append(funcStr.split("_")[1])
+
+    #debug
+#    print("    exploitCode Data...")
+#    print("        functionList : ",funcStrList)
+    syscallStrList = FigureSyscall(funcStrList,linux_syscallList)
+
+    #debug
+#    print("        syscallStrList : ",syscallStrList)
+    return syscallStrList
+
+#find system calls in cve description and return refered system call list
+def FindCVEDescSyscallStrList(cveName, linux_syscallList):
+    funcStrList = list()
     syscallStrList = list()
+    fFormat=re.compile('''[A-Za-z_][0-9A-Za-z_]+\(.*\)''')
+    
+    webSite = "https://cve.mitre.org/cgi-bin/cvename.cgi?name="+cveName
+    pathHtml = requests.get(webSite)
+    soup = BeautifulSoup(pathHtml.content, 'html.parser')
+    webTextData = soup.find_all('tr')
+    i=0
+    for i in range(0,len(webTextData)):
+        if 'Description' == webTextData[i].text.strip("\n"):
+            cveDesc = webTextData[i+1].text
+        i+=1
+
+    for funcStr in fFormat.findall(cveDesc):
+        funcStrList.append(funcStr.split("(")[0])
+
+    fFormat_sys = re.compile('''SYS_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_sys.findall(cveDesc):
+        funcStrList.append(funcStr.split("_")[1])
+
+    fFormat_NR = re.compile('''NR_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_NR.findall(cveDesc):
+        funcStrList.append(funcStr.split("_")[1])
+
+    #debug
+#    print("    cve description data ...")
+#    print("        functionList : ",funcStrList)
+    syscallStrList = FigureSyscall(funcStrList,linux_syscallList)
+    syscallStrList.extend(FigureSyscall(cveDesc.split(" "),linux_syscallList))
+    #debug
+#    print("        syscallStrList : ", syscallStrList)
+    return syscallStrList
+
+#find system calls in exploit title and return refered system call to list
+def FindTitleSyscallStrList(title, linux_syscallList):
+    funcStrList = list()
+    fFormat=re.compile('''[A-Za-z_][0-9A-Za-z_]+\(.*\)''')
+    for funcStr in fFormat.findall(title):
+        funcStrList.append(funcStr.split("(")[0])
+
+    fFormat_sys = re.compile('''SYS_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_sys.findall(title):
+        funcStrList.append(funcStr.split("_")[1])
+
+    fFormat_NR = re.compile('''NR_[A-Za-z_0-9]+''',re.I)
+    for funcStr in fFormat_NR.findall(title):
+        funcStrList.append(funcStr.split("_")[1])
+
+    #debug
+#    print("    exploit title ...")
+#    print("        functionList : ",funcStrList)
+    FigureSyscall(title.split(" "),linux_syscallList)
+    syscallStrList = FigureSyscall(funcStrList,linux_syscallList)
+    syscallStrList.extend(FigureSyscall(title.split(" "),linux_syscallList))
+    
+    #debug
+#    print("        syscallStrList : ", syscallStrList)
+
+    return syscallStrList
+
+def AddCrawlingInfo2Dict(initDict):
+    #fake header to get reponse packet from web site
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+    
     exploitDict = copy.deepcopy(initDict)
+
+    #linux system call list in docker
+    linux_syscallList = GetLinuxSyscallList()
+    #regular expression to get cve name from exploit code web page
     cveFormat = re.compile('.*(CVE-[0-9]*-[0-9]*).*',re.I)
+    
+    #loop for each exploit ID 
     for exploitID, informDict in exploitDict.items():
+        #for save found systemcall string in the exploit information
+        syscallStrList = list()
+        #request exploit code web page in exploit-db
         pathHtml = requests.get(informDict['codePath'],headers=headers)
+
+        #set beautifulsoup and get exploit code and cve name
         soup = BeautifulSoup(pathHtml.content, 'html.parser')
         codeStr  = soup.find('code').text
         searchRet = cveFormat.search(soup.find('meta',attrs={'name':'keywords'}).get('content'))
@@ -131,15 +234,28 @@ def AddDBExploit2Dict(initDict):
         if searchRet is not None :
             relatedCVE = searchRet.group(1)
         exploitDict[exploitID]['relatedCVE'] = relatedCVE
-        GetLinuxSyscallList()
-#        syscallStrList= FindSyscallStrList(codeStr)
-#        exploitDict[exploitID]['syscallStrList'] = syscallStrList
+        
+        #debug
+        print("exploitID : ",exploitID)
+        print("    relatedCVE : ",relatedCVE)
+        
+        #find system calls in exploit code and add to list
+        syscallStrList.extend(FindCodeSyscallStrList(codeStr,linux_syscallList))
+        
+        #find system calls in cve description and add to list
+        if relatedCVE != "":
+            syscallStrList.extend(FindCVEDescSyscallStrList(relatedCVE,linux_syscallList))
+        #find system calls in exploit title
+        exploitTitle = informDict['title']
+        syscallStrList.extend(FindTitleSyscallStrList(exploitTitle,linux_syscallList))
+
+        #add sytemcall string list, related cve name to exploit dict
+        exploitDict[exploitID]['syscallStrList'] = list(set(syscallStrList))
+        print("    related system call : ", exploitDict[exploitID]['syscallStrList'])
     return exploitDict
 
-def AddCveExploit2Dict(exploitDict):
-    for exploitID, informDict  in exploitDict.items():
-        codeStr = exploitDict[exploitID]['codeStr']
-    return exploitDict
+
+#save exploit dict to pickle save file
 def SaveExploitDict(exploitDict,path):
     with open(path,"wb") as f:
         pickle.dump(exploitDict, f)
@@ -156,15 +272,13 @@ if __name__ == "__main__":
         os.system("rm /opt/volume/initDict*")
         SaveExploitDict(initDict, initDictPath)
     
-    exploitDict = AddDBExploit2Dict(initDict)
+    exploitDict = AddCrawlingInfo2Dict(initDict)
     exploitDictPath = "/opt/volume/exploitDict_"+today+".sav"
 
 #    if os.path.exists(exploitDictPath):
 #        with open(exploitDictPath,"rb") as f:
 #            exploitDict = pickle.load(f)
 #    else:
-#        exploitDict = AddDBExploit2Dict(initDict)
+#        exploitDict = AddCrawlingInfo2Dict(initDict)
 #        os.system("rm /opt/volume/exploitDict*")
 #        SaveExploitDict(exploitDict, exploitDictPath)
-
-
